01f396b20fb2a203bb7178fa861a126137fe886d
Wenrong Deng <dwrandy520@gmail.com>
2019-01-23 14:58:11 -0700

    Changed favicon, add two components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BasicCalculatorComponent } from './basic-calculator/basic-calculator.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', component: BasicCalculatorComponent },
-  { path: 'basic-calculator', component: BasicCalculatorComponent }
+  { path: 'home', component: HomePageComponent },
+  { path: 'basic-calculator', component: BasicCalculatorComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared-module/material-module';
 import { BasicCalculatorComponent } from './basic-calculator/basic-calculator.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicCalculatorComponent
+    BasicCalculatorComponent,
+    PageNotFoundComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
