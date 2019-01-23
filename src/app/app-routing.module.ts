d37501b435fd548f5cda92b32ce5d0292f6324db
Wenrong Deng <dwrandy520@gmail.com>
2019-01-23 13:23:30 -0700

    Build attempt n

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { BasicCalculatorComponent } from './basic-calculator/basic-calculator.component';
 
 const routes: Routes = [
-  { path: 'iPortfolio', component: BasicCalculatorComponent },
+  { path: '', component: BasicCalculatorComponent },
   { path: 'basic-calculator', component: BasicCalculatorComponent }
 ];
 
