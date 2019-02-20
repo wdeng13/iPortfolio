aaf2757bcb8e7de715eafaa76435a890628b30b1
Wenrong Deng <dwrandy520@gmail.com>
2019-02-20 00:07:18 -0700

    Edited experience and skills

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -10,14 +10,14 @@ export class SkillComponent implements OnInit {
   TECHNICAL_SKILLS = [
     { name: 'Python, Java, TypeScript', level: 80 },
     { name: 'C++, C, JavaScript, HTML & CSS', level: 70 },
-    { name: 'C#', level: 60 },
-    { name: 'Angular', level: 80 }
-  ]
+    { name: 'Swift, C#, SQL', level: 65 },
+    { name: 'Angular, GIT, Node.js, Axure RP', level: 75 }
+  ];
 
   OTHER_SKILLS = [
     { name: 'Chinese (Mandarin)', level: 90 },
     { name: 'Chinese (Cantonese)', level: 100 },
-  ]
+  ];
 
   constructor() { }
 
